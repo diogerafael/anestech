de215cab14fd3534716a0039041f6daa6de8707e
diogenes.rafael <diogenes.rafael@crptecnologia.com.br>
2020-11-13 22:25:14 -0300

    Create test and middlleware auth

---
+++ src/middlewares/__test__/auth.test.ts
@@ -0,0 +1,69 @@
+import AuthService from '@src/services/auth';
+import { authMiddleware } from '../auth';
+
+describe('AuthMiddleware', () => {
+    it('should verify a JWT token and call the next middleware', () => {
+        const jwtToken = AuthService.generateToken({ data: 'fake' });
+        const reqFake = {
+            headers: {
+                'x-access-token': jwtToken,
+            },
+        };
+
+        const resFake = {};
+        const nextFake = jest.fn();
+        authMiddleware(reqFake, resFake, nextFake);
+
+        //verifica se funcao foi passada
+        expect(nextFake).toHaveBeenCalled();
+    });
+
+
+    it('should return UNAUTHORIZED if there is a problem in token verification', () => {
+        const reqFake = {
+            headers: {
+                'x-access-token': 'invalid token'
+            }
+        };
+
+        const sendMock = jest.fn();
+        const resFake = {
+            status: jest.fn(() => ({
+                send: sendMock,
+            })),
+        };
+
+        const nextFake = jest.fn();
+        authMiddleware(reqFake, resFake as object, nextFake);
+        expect(resFake.status).toHaveBeenCalledWith(401);
+        expect(sendMock).toHaveBeenCalledWith({
+            code: 401,
+            error: 'jwt malformed',
+        });
+
+    });
+
+
+    it('should return Anauthorized middleware if there no token', () => {
+        const reqFake = {
+            headers: {}
+        };
+
+        const sendMock = jest.fn();
+        const resFake = {
+            status: jest.fn(() => ({
+                send: sendMock,
+            })),
+        };
+
+        const nextFake = jest.fn();
+        authMiddleware(reqFake, resFake as object, nextFake);
+        expect(resFake.status).toHaveBeenCalledWith(401);
+        expect(sendMock).toHaveBeenCalledWith({
+            code: 401,
+            error: 'jwt malformed',
+        });
+
+    })
+
+});
